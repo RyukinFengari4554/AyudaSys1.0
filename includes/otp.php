9f0245bbfcbbaacf8442d00059a039aa9c15fe6f
RyukinFengari4554 <80999560+RyukinFengari4554@users.noreply.github.com>
2022-05-05 23:56:19 +0800

    master

---
+++ includes/otp.php
@@ -0,0 +1,54 @@
+<?php
+    include_once 'db.inc.php';
+    include 'register.inc.php';
+    
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Phone Verification</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" media="all" href="https://cdnjs.cloudflare.com/ajax/libs/authy-form-helpers/2.3/form.authy.min.css">
+    </head>
+
+    <body>
+        <header class="site-home-header">
+            <div class="outer site-header-background no-image">
+                <div class="inner">
+                    <div class="site-header-content">
+                        <h1 class="site-title">
+                            </h1>
+                        <h2 class="site-description">Phone Verification OTP</h2>
+                        </div>
+                </div>
+            </div>
+        </header>
+        <form action="verify.php" accept-charset="UTF-8" method="POST">
+            <div class="container">
+                <p style="color: white; text-align:center; font-size: 7.5rem;"><?php echo $contact_number;?> </p>
+                <p class="channel" id="channel" name="ch" value="sms" style="visibility: hidden"></p>
+                <select class="select" id="length" name="l" style="visibility: hidden">
+                    <option value="4">4</option>
+                    <option value="6" selected>6</option>
+                    <option value="8">8</option>
+                </select>
+                <button name="button" type="submit">SEND SMS</button>
+                <table class="center" style="visibility: hidden">
+                <tr>
+                  <td><select name="cc" id="authy-countries"  data-show-as="text"></select></td>
+                  <td><input type="tel" id="phone_number" placeholder="Phone Number" name="pn" maxlength="10" value=<?php echo $vcn;?>></td>
+                  <!--
+                  <td><p type="tel" id="phone_number" name="pn" maxlength="10">
+                      <?php //echo $vcn;
+                      ?>
+                      </p>
+                      </td>
+-->
+                </tr>
+              </table>
+            </div>
+        </form>
+          <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/authy-form-helpers/2.3/form.authy.min.js"></script> -->
+        <script src="newcountry.js" charset="utf-8"></script>
+    </body>
+</html>
--- includes/register.inc.php
+++ includes/register.inc.php
@@ -24,23 +24,48 @@
     $barangay = mysqli_real_escape_string($conn, $_POST['rb']);
     $package = mysqli_real_escape_string($conn, $_POST['rp']);
     $qr_code = generateRandomString();
-    // GRANTING PROCESS:
+    // GRANTING PART 1:
     $vcn= substr($contact_number, 1); //VERYFING OTP NUMBER TESTING FOR PARSING
     $sql = "SELECT * FROM personal_information WHERE barangay_id = '$barangay_id' AND first_name = '$first_name' AND middle_name = '$middle_name' AND last_name = '$last_name' AND no_of_members = $no_of_members;";
     $result = mysqli_query($conn, $sql);
     $RC = mysqli_num_rows($result);
-    if ($RC > 0 ){
-        $v_s = "Granted";
-        
+    //getting FAMILY_CODE from PERSONAL TABLE
+    $row = mysqli_fetch_assoc($result);
+    $fam_code = $row['family_code'];
+
+    if ($RC > 0 ){ // checking if inputted data are IN personal_info Table
+        $sql = "SELECT * FROM granted WHERE family_code = '$fam_code';";
+        $result = mysqli_query($conn, $sql);
+        $RC = mysqli_num_rows($result);
+        if ($RC == 0 ){ // checking if inputted data are IN granted Table
+            $v_s = "Granted";
+            
+            /*
+            $start_date=date('y/m/d');
+            $pud= date("y/m/d", strtotime("$start_date +1 week"));
+            */
+            $start_date=date('y/m/d');
+            $startdate = $_POST['start_date'];
+            $pud = date('Y/m/d',strtotime('+1 week',$startdate));
+        }
+        else{
+            $v_s = "Denied";
+        }
     }
     else{
         $v_s = "Denied";
     }
     
 
-    
+    //INSERTING DATA INTO REGISTRATION TABLE
     $sql = "insert into registration (barangay_id, house_no, first_name, middle_name, last_name, no_of_members, email, contact_number, street, barangay, verification_status, package, qr_code) values ('$barangay_id', '$house_no', '$first_name', '$middle_name', '$last_name', '$no_of_members', '$email', '$contact_number', '$street', '$barangay', '$v_s', '$package', '$qr_code');";
     $result = mysqli_query($conn, $sql);
+    
+    //getting REGISTATION_NO from REGISTRATION TABLE
+    $sql = "SELECT * FROM registration WHERE qr_code = '$qr_code';";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $reg_no = $row['registration_no'];
     
     /* include QRBarCode class */
     include "QRBarCode.php";
@@ -55,18 +80,26 @@
          return $this->qr_code;
         }
     }
-    $start_date=date('d/m/y');
-    $pud= date("d/m/y", strtotime("$start_date +1 week"));
-    if ($RC > 0 ){
-        if ($package=="Package A: Cash"){
-            $package_no=1;
-        }
-        else{
-            $package_no=2;
-        }
-        $distribution_status=false;
-        //$sql = "insert into granted (qr_code, granted_date, pick_up_date, barangay_id, package_no, distrbution_status) values ('$qr_code', '$start_date', '$pud', '$barangay_id', '$package_no', '$distribution_status');";
-        
+    //GRANTING PART 2:
+    $sql = "SELECT * FROM personal_information WHERE barangay_id = '$barangay_id' AND first_name = '$first_name' AND middle_name = '$middle_name' AND last_name = '$last_name' AND no_of_members = $no_of_members;";
+    $result = mysqli_query($conn, $sql);
+    $RC = mysqli_num_rows($result);
+    if ($RC > 0 ){ // checking if inputted data are IN personal_info Table
+        $sql = "SELECT * FROM granted WHERE family_code = '$fam_code';";
+        $result = mysqli_query($conn, $sql);
+        $RC = mysqli_num_rows($result);
+        if ($RC == 0 ){ // checking if inputted data are IN granted Table
+            if ($package=="Package A: Cash"){
+                $package_no=1;
+            }
+            else{
+                $package_no=2;
+            }
+            $distribution_status=false;
+            //$sql = "insert into granted (qr_code, registration_no, granted_date, pick_up_date, barangay_id, family_code, package_no, distrbution_status) values ('$qr_code', '$reg_no', '$start_date', '$pud', '$barangay_id', '$fam_code','$package_no', '$distribution_status');";
+            //$result = mysqli_query($conn, $sql);
+            header("Location: otp.php");
+            exit();
         /* COMMENTED on 4/14/2022
         $sql = "insert into granted (qr_code, granted_date, pick_up_date, distrbution_status) values ('$qr_code', '$start_date', '$pud', '$distribution_status');";
         $result = mysqli_query($conn, $sql);
@@ -80,58 +113,13 @@
                 insert INFO (from reg. table) -> granted table 
 
         */
+        }
+        else{
+            header("Location: ../failed_registered_already.php");
+        }
     }
     else{
-        //gawa ng FAILED EIther one of your information is incorrect or you're not in the database.
+        header("Location: ../failed_registered_already.php");
     }
 ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Phone Verification</title>
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" media="all" href="https://cdnjs.cloudflare.com/ajax/libs/authy-form-helpers/2.3/form.authy.min.css">
-    </head>
-
-    <body>
-        <header class="site-home-header">
-            <div class="outer site-header-background no-image">
-                <div class="inner">
-                    <div class="site-header-content">
-                        <h1 class="site-title">
-                            </h1>
-                        <h2 class="site-description">Phone Verification OTP</h2>
-                        </div>
-                </div>
-            </div>
-        </header>
-        <form action="verify.php" accept-charset="UTF-8" method="POST">
-            <div class="container">
-                <p style="color: white; text-align:center; font-size: 7.5rem;"><?php echo $contact_number;?> </p>
-                <p class="channel" id="channel" name="ch" value="sms" style="visibility: hidden"></p>
-                <select class="select" id="length" name="l" style="visibility: hidden">
-                    <option value="4">4</option>
-                    <option value="6" selected>6</option>
-                    <option value="8">8</option>
-                </select>
-                <button name="button" type="submit">SEND SMS</button>
-                <table class="center" style="visibility: hidden">
-                <tr>
-                  <td><select name="cc" id="authy-countries"  data-show-as="text"></select></td>
-                  <td><input type="tel" id="phone_number" placeholder="Phone Number" name="pn" maxlength="10" value=<?php echo $vcn;?>></td>
-                  <!--
-                  <td><p type="tel" id="phone_number" name="pn" maxlength="10">
-                      <?php //echo $vcn;
-                      ?>
-                      </p>
-                      </td>
--->
-                </tr>
-              </table>
-            </div>
-        </form>
-          <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/authy-form-helpers/2.3/form.authy.min.js"></script> -->
-        <script src="newcountry.js" charset="utf-8"></script>
-    </body>
-</html>
