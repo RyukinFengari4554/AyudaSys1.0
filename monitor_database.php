0b96a7b30b4912143102a17d41e0e7067ef52136
RyukinFengari4554 <reibun.morikyo@gmail.com>
2022-05-21 22:20:01 +0800

    main

--- monitor_database.php
+++ monitor_database.php
@@ -13,78 +13,98 @@ if(empty($_SESSION['sun']) || $account=="login-failed"){
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="/docs/4.1/assets/img/favicons/favicon.ico">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="/docs/4.1/assets/img/favicons/favicon.ico">
 
-    <title>AyudaSys Monitor Database</title>
+  <title>AyudaSys Monitor Database</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.1/examples/sign-in/">
+  <link rel="canonical" href="https://getbootstrap.com/docs/4.1/examples/sign-in/">
 
-    <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Bootstrap -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+  <!-- Bootstrap core CSS -->
+  <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+    integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js"
+    integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous">
+  </script>
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,700;0,900;1,300;1,400&family=Ubuntu:wght@700&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,700;0,900;1,300;1,400&family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,700;0,900;1,300;1,400&family=Ubuntu:wght@700&display=swap"
+    rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,700;0,900;1,300;1,400&family=Ubuntu:wght@300;400;500;700&display=swap"
+    rel="stylesheet">
 
 
 
 
-    <!-- Custom styles for this template -->
-    <link href="styles/adminpage.css" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="styles/adminpage.css" rel="stylesheet">
 
 </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-dark">
-      <a class="navbar-brand brand-title" href="index.php">AyudaSys </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link links" href="includes/home_check.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link links" href="includes/logout.php">Log out</a>
-          </li>
-        </ul>
-    </nav>
 
-    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-      <h1 class="display-4">Monitor Registry Page</h1>
-      <p class="lead">The Monitor Registry Page allows for monitoring of the database list</p>
-    </div>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark">
+    <a class="navbar-brand brand-title" href="index.php">AyudaSys </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link links" href="includes/home_check.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link links" href="includes/logout.php">Log out</a>
+        </li>
+      </ul>
+  </nav>
+
+  <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+    <h1 class="display-4">Monitor Registry Page</h1>
+    <p class="lead">The Monitor Registry Page allows for monitoring of the database list</p>
+  </div>
 
 
-    <div class="container">
-      <div class="card-deck  text-center">
-        <div class="card  light-sm">
-            <a  href="citizen_info.php"><button type="button" class="btn btn-lg btn-block btn-outline-primary">View Citizen Info</button></a>
-        </div>
-        <div class="card  light-sm">
-            <a href="granted_table.php"><button type="button" class="btn btn-lg btn-block btn-outline-primary">View Granted Table</button></a>
-        </div>
-        <div class="card  light-sm">
-          <a  href="viewaccounts.php"><button type="button" class="btn btn-lg btn-block btn-outline-primary">View Barangay Official accounts</button></a>
+  <div class="container">
+    <div class="card-deck  text-center">
+      <div class="card  light-sm">
+        <a href="citizen_info.php"><button type="button" class="btn btn-lg btn-block btn-outline-primary">View
+            <br>Citizen Info</button></a>
       </div>
       <div class="card  light-sm">
-          <a  href="monitor_registry.php"><button type="button" class="btn btn-lg btn-block btn-outline-primary">View <br>Registry List </button></a>
+        <div class="card  light-sm">
+          <a href="monitor_registry.php"><button type="button" class="btn btn-lg btn-block btn-outline-primary">View
+              <br>Registry List </button></a>
+        </div>
+        <a href="granted_table.php"><button type="button" class="btn btn-lg btn-block btn-outline-primary">View
+            <br>Granted Table</button></a>
       </div>
       <div class="card light-sm">
-          <a  href="manage_necessity.php"><button type="button" class="btn btn-lg btn-block btn-outline-primary">Show Ayuda <br>Packages List<List></List></button></a>
+        <a href="manage_necessity.php"><button type="button" class="btn btn-lg btn-block btn-outline-primary">Show Ayuda
+            <br>Packages List<List></List></button></a>
       </div>
+      <div class="card  light-sm">
+        <a href="viewaccounts.php"><button type="button" class="btn btn-lg btn-block btn-outline-primary">View Barangay
+            Official accounts</button></a>
       </div>
 
 
+    </div>
+  </div>
 
 
-  </body>
+</body>
+
 </html>
