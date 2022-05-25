4ee5cf9a6acf05456e5de2fb5805c9073f10b3c2
RyukinFengari4554 <reibun.morikyo@gmail.com>
2022-05-25 21:38:04 +0800

    main

--- index.php
+++ index.php
@@ -153,6 +153,7 @@
     </nav>
 
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+    <img src="images/logow2.png" style="width: 30%; height: 30%; margin: 0;"></img>
       <h1 class="display-4">AyudaSys</h1>
       <p class="lead" id="d1">AyudaSys is a newly Digitalized Barangay Ayuda Management System used to assist barangay
         officials to gather the basic necessities of every household in their respective barangay.</p>
@@ -176,7 +177,7 @@
   <section id="d2">
     <!-- Nav Bar -->
     <nav class="navbar navbar-expand-lg navbar-dark">
-      <a class="navbar-brand brand-title" href="index.php"><img src="images/logow2.png" style="width: 15%; height: 15%;"></img>AyudaSys </a>
+      <a class="navbar-brand brand-title" href="index.php">AyudaSys </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -200,7 +201,7 @@
     </nav>
 
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-    <img src="images/logow2.png" style="width: 25%; height: 25%;"></img>
+    <img src="images/logow2.png" style="width: 30%; height: 30%; margin: 0;"></img>
       <h1 class="display-4">AyudaSys</h1>
       <p class="lead">AyudaSys ay isang Makabagong Sistema na Namamahala sa Barangay Ayuda na ginagamit upang tulungan
         ang mga opisyal ng barangay na tipunin ang mga pangunahing pangangailangan ng bawat sambahayan sa kani-kanilang
