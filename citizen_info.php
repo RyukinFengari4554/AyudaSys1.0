6759392547905bc0c45512915e4c0d47b117895e
RyukinFengari4554 <reibun.morikyo@gmail.com>
2022-05-30 14:44:20 +0800

    main

--- citizen_info.php
+++ citizen_info.php
@@ -195,6 +195,7 @@ table{
     </div>
 
     <div id="t2">
+      <?php
       if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };
       $results_per_page = 20;
       $start_from = ($page-1) * $results_per_page;
