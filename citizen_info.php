ab09d29b462fdbcde20c02b543d8f6eb086ce23a
RyukinFengari4554 <reibun.morikyo@gmail.com>
2022-05-31 15:59:49 +0800

    main

--- citizen_info.php
+++ citizen_info.php
@@ -14,8 +14,7 @@ include 'includes/db.inc.php';
 <html lang="en">
 
 <head>
-    <!--<meta charset="utf-8"> -->
-    <meta charset="utf8mb4">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
--- includes/db.inc.php
+++ includes/db.inc.php
@@ -17,4 +17,6 @@ $active_group='default';
 $query_builder=TRUE;
 // Connect to DB
 $conn=mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+mysql_set_charset('utf8', $conn);
 ?>     
