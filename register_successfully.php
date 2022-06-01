fe477e30c4c92435b06262a603664774f7e759a5
RyukinFengari4554 <reibun.morikyo@gmail.com>
2022-06-02 00:46:45 +0800

    main

--- register_successfully.php
+++ register_successfully.php
@@ -243,9 +243,10 @@
         $run = escapeshellcmd("includes/print_qr_printer.py \"$qr_code\"");
         $out = shell_exec($run);
         */
-        //$output = passthru('python test.py $qr_code');
+        /*$output = passthru('python test.py $qr_code');
         $output = escapeshellcmd("test.py \"$qr_code\"");
-        //$run = escapeshellcmd("includes/print_qr_printer.py \"$qr_code\"");
+        //$run = escapeshellcmd("includes/print_qr_printer.py \"$qr_code\""); */
+        exec("nohup sudo python test.py ".$qr_code);
         echo 'alert('.$output.');';
     ?>
   }
