43919bb4ac444e1c8cd67c3eae62aee0b779a038
RyukinFengari4554 <reibun.morikyo@gmail.com>
2022-06-03 20:36:46 +0800

    main

--- register_successfully.php
+++ register_successfully.php
@@ -51,7 +51,9 @@
   <link
     href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,700;0,900;1,300;1,400&family=Ubuntu:wght@300;400;500;700&display=swap"
     rel="stylesheet">
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Seymour+One&display=swap" rel="stylesheet">
 
   <!-- Add icon library -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
