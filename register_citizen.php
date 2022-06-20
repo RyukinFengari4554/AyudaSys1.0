94e8189cb591d259a9490cac975a86d1d079281c
RyukinFengari4554 <reibun.morikyo@gmail.com>
2022-06-20 11:15:56 +0800

    main

--- register_citizen.php
+++ register_citizen.php
@@ -71,10 +71,10 @@ if(empty($_SESSION['sun']) || $account=="login-failed"){
       <?php
     $fulUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
                   if (strpos($fulUrl,"register_citizen.php?register=success") == true){
-                    echo "<p style='color: green; margin: auto;'>Citizen Registration Success</p>";
+                    echo "<h2 style='color: green; margin: auto;'>Citizen Registration Success</h2>";
                   };
                   if (strpos($fulUrl,"register_citizen.php?register=failed") == true){
-                    echo "<p style='color: red;margin: auto;'>Citizen Registration Failed</p>";
+                    echo "<h2 style='color: red;margin: auto;'>Citizen Registration Failed</h2>";
                   };
       ?>
     </div>
