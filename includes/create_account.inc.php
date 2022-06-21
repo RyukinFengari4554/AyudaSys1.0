2b43fe0f66faecbdad00ac8d38d49e4ef0c0cf90
RyukinFengari4554 <reibun.morikyo@gmail.com>
2022-06-22 00:56:02 +0800

    main

--- includes/create_account.inc.php
+++ includes/create_account.inc.php
@@ -12,6 +12,7 @@
     if($RC > 0){
         header("Location: ../create_barangay_official_accout.php?creation=failed");
     }
+    else{
     $sql = "SELECT * FROM barangay_officials WHERE username='$user_n' AND password='$pas' AND barangay='$brngy';";
     $result = mysqli_query($conn, $sql);
     $RC = mysqli_num_rows($result);
@@ -23,5 +24,5 @@
         $result = mysqli_query($conn, $sql);
         header("Location: ../create_barangay_official_accout.php?creation=success");
     }
-   
+}
 ?>
