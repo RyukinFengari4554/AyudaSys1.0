fdfb552b7212e1b8c8488c9445048b31e2a988f2
RyukinFengari4554 <reibun.morikyo@gmail.com>
2022-07-04 21:55:18 +0800

    main

--- create_barangay_official_accout.php
+++ create_barangay_official_accout.php
@@ -139,7 +139,7 @@ function myFunction() {
   }
   else{
     //alert('FAILED');
-    window.location.href("create_barangay_official_accout.php?password=wrong");
+    window.location.replace("create_barangay_official_accout.php?password=wrong");
   }
 }
 </script>
