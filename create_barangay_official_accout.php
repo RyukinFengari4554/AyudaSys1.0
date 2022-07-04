9253f62bcd4e0652168419368d32ad5cc631e018
RyukinFengari4554 <reibun.morikyo@gmail.com>
2022-07-04 22:22:42 +0800

    main

--- create_barangay_official_accout.php
+++ create_barangay_official_accout.php
@@ -120,10 +120,10 @@ if(empty($_SESSION['sun']) || $account=="login-failed" || $account=="barangay"){
     <br>
     
     <br>
-    <button class="w-100 btn btn-primary " id="myBtn" type="submit" hidden>Create</button>
+    <button class="w-100 btn btn-primary " id="myBtn" type="submit" hidden>Submit</button>
   
   </form>
-  <button class="w-100 btn btn-primary " id="myBtn1" onclick="myFunction();">Submit</button>
+  <button class="w-100 btn btn-primary " id="myBtn1" onclick="myFunction();">Create</button>
   <p> </p>
 <a href="includes/home_check.php"><button class="w-100 btn btn-primary " type="submit" id="btns">Return Home</button></a>
 <p>&zwnj;</p>
