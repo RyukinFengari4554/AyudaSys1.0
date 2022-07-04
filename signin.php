c6818adbb5c7af5205353fa7cc7a436cb352873d
RyukinFengari4554 <reibun.morikyo@gmail.com>
2022-07-05 01:45:29 +0800

    main

--- signin.php
+++ signin.php
@@ -101,7 +101,7 @@ if(!empty($_SESSION['sun'])){
       <div class="card-deck  text-center">
         <div class="card  light-sm">
         <a href="index.php"><button type="button" id="d1"
-              class="btn btn-lg btn-block btn-outline-primary">Return Home</button></a>
+              class="btn btn-sm btn-block btn-outline-primary">Return Home</button></a>
         </div>
       </div>
     </div>
