3a217df6f12bfa50499ba1673ec6f4f05a8c946d
RyukinFengari4554 <reibun.morikyo@gmail.com>
2022-07-04 16:01:46 +0800

    main

--- distribute.php
+++ distribute.php
@@ -78,6 +78,7 @@ if(empty($_SESSION['sun']) || $account=="login-failed"){
         <div>
           <form name="form" action="distribute_ayuda.php" method="POST">
             <!--<label>SCAN QR CODE</label>-->
+            <br>
             <input style="font-size: 2rem; text-align: center;" type="text" name="qrs" id="qrs" placeholder="scan qrcode" class="form-control" readonly> <!--cmnt 27/04/22 TODO Error handling for empty inputbox also enlarge input box or remove from table form -->
             <br>
             <button class="w-100 btn btn-primary " type="submit">Submit</button>
